3a912395ee7b554f31c2f2c420275e684007c385
William Tomé <william.tomenh@gmail.com>
2021-03-15 21:37:55 -0300

    Using getServerSideProps.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-const Index = () => {
+const Index = (props) => {
   return (
     <div>
       <h1>Hello world</h1>
+      <div>
+          <h3>{props.currentDate}</h3>
+      </div>
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      currentDate: new Date().toString(),
+    },
+  };
+}
 
 export default Index;
