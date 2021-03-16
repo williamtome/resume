b3fd0274554093e7802829b22564df0565f06a29
William Tomé <william.tomenh@gmail.com>
2021-03-15 22:06:18 -0300

    List my repos.

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,29 @@ const Index = (props) => {
     <div>
       <h1>Hello world</h1>
       <div>
-          <h3>{props.currentDate}</h3>
+        <h3>{props.currentDate}</h3>
+      </div>
+      <div>
+        {props.repos.map(repo => {
+          return (
+            <div>
+              <h3>{repo.full_name}</h3>
+              <p>Language: {repo.language} - Stars: {repo.stargazers_count}</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
 };
 
 export async function getServerSideProps(context) {
+  const resRepos = await fetch('https://api.github.com/users/williamtome/repos?sort=updated')
+  const repos = await resRepos.json()
   return {
     props: {
       currentDate: new Date().toString(),
+      repos
     },
   };
 }
