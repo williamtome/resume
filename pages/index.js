1fa0a88d18cc6d8a57ed2aa700b9c4e6a162dcd1
William Tomé <william.tomenh@gmail.com>
2021-03-16 22:31:59 -0300

    Filtering data received from GitHub API and display on screen.

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,11 @@ const Index = (props) => {
       <div>
         <h3>{props.currentDate}</h3>
       </div>
+      {/* <pre>{JSON.stringify(props.repos, null, 2)}</pre> */}
       <div>
         {props.repos.map(repo => {
           return (
-            <div>
+            <div key={repo.id}>
               <h3>{repo.full_name}</h3>
               <p>Language: {repo.language} - Stars: {repo.stargazers_count}</p>
             </div>
@@ -23,7 +24,29 @@ const Index = (props) => {
 
 export async function getServerSideProps(context) {
   const resRepos = await fetch('https://api.github.com/users/williamtome/repos?sort=updated')
-  const repos = await resRepos.json()
+  const originalRepos = await resRepos.json()
+
+  const dontShowRepos = [
+    'williamtome/form-validation-laravel',
+    'williamtome/laravel-advanced-form-validation',
+    'williamtome/happy-api',
+    'williamtome/happy'
+  ]
+
+  const isNotFork = repo => !repo.fork
+  const showRepos = repo => dontShowRepos.indexOf(repo.full_name) === -1
+  const extractData = repo => ({
+    id: repo.id,
+    full_name: repo.full_name,
+    language: repo.language,
+    stargazers_count: repo.stargazers_count
+  })
+
+  const repos = originalRepos
+                  .filter(isNotFork)
+                  .filter(showRepos)
+                  .map(extractData)
+  
   return {
     props: {
       currentDate: new Date().toString(),
