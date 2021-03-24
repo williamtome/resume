8830f78946a456773349a053d0ee88806c5c8452
William Tomé <william.tomenh@gmail.com>
2021-03-23 21:45:48 -0300

    Get GitHub repositories and user data..

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import getUser from "../utils/getUser"
 
 const Index = ({ currentDate, repos, user }) => {
   return (
@@ -24,33 +25,7 @@ const Index = ({ currentDate, repos, user }) => {
 };
 
 export async function getServerSideProps(context) {
-  const resUser = await fetch('https://api.github.com/users/williamtome')
-  const user = await resUser.json()
-  
-  const resRepos = await fetch('https://api.github.com/users/williamtome/repos?sort=updated')
-  const originalRepos = await resRepos.json()
-
-  const dontShowRepos = [
-    'williamtome/form-validation-laravel',
-    'williamtome/laravel-advanced-form-validation',
-    'williamtome/happy-api',
-    'williamtome/happy'
-  ]
-
-  const isNotFork = repo => !repo.fork
-  const showRepos = repo => dontShowRepos.indexOf(repo.full_name) === -1
-  const extractData = repo => ({
-    id: repo.id,
-    full_name: repo.full_name,
-    language: repo.language,
-    stargazers_count: repo.stargazers_count
-  })
-
-  const repos = originalRepos
-                  .filter(isNotFork)
-                  .filter(showRepos)
-                  .map(extractData)
-  
+  const {repos, user} = await getUser('williamtome')
   return {
     props: {
       currentDate: new Date().toString(),
